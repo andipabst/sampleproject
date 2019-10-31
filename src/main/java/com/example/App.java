0f3da4d47f03d50274428b84ed18f50df4516411
andi <andipabst25@gmail.com>
2019-10-31 13:19:25 +0100

    initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.util.Map;
+import java.lang.String;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        DeprecatedClass.deprecatedMethod();
+    }
+
+    public void untestedMethod() {
+        System.out.println("This is not tested");
+    }
+
+    public void emptyIf() {
+        if (true) {
+            
+        }
+    }
+}
---
+++ src/main/java/com/example/DeprecatedClass.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.Map;
+
+public class DeprecatedClass {
+    
+    @Deprecated
+    public static void deprecatedMethod() {
+        // nothing to see here
+    }
+}
